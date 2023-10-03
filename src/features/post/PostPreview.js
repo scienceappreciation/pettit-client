a0eea50edbfa5bd40f2675ab15a7996f997230ba
scienceappreciation <colehale059@gmail.com>
2023-10-03 09:56:18 -0500

    Adjust PostsContainer formatting

--- src/features/post/PostPreview.js
+++ src/features/post/PostPreview.js
@@ -1,11 +1,12 @@
 import React from 'react';
 
-function PostPreview() {
+function PostPreview({ title, body, author, upvotes }) {
     return (
         <article className='border-container'>
             <h2>{title}</h2>
             <div className='border-container'>
-                {body}
+                {body.text}
+                {body.image_src.length > 0 ? <img alt={title} src={body.image_src} /> : ""}
             </div>
             <ul className='meta-container'>
                 <li>
--- src/features/post/PostsContainer.js
+++ src/features/post/PostsContainer.js
@@ -1,17 +1,19 @@
 import React from 'react';
+import { PostPreview } from './PostPreview.js';
 
-function PostsContainer({posts}) {
-    function renderPosts(elements) {
+function PostsContainer({ posts }) {
+    function renderPosts() {
         let result = [];
-        for (const post of elements) {
-            result.push(post);
+        for (const post of posts) {
+            result.push(<PostPreview title={post.title} body={post.content} author={post.author} upvotes={post.upvotes}/>)
         }
         return result;
     }
 
+
     return (
         <div className='container'>
-            { renderPosts(posts) }
+            { renderPosts()  }
         </div>
     );
 }
