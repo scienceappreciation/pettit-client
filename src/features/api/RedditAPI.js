64064f48985ccee1960c1c406f8582cde9f88a64
scienceappreciation <colehale059@gmail.com>
2023-10-05 09:27:57 -0500

    Create Mock Listing Fetch

--- src/features/api/RedditAPI.js
+++ src/features/api/RedditAPI.js
@@ -1,9 +1,17 @@
 import PostData from "../../classes/PostData";
 import Listing from "../../classes/Listing";
-
-const BASE_URL = "https://api.reddit.com";
+const BASE_URL = "https://reddit.com/";
 const SUB_URL = "/r/";
 const POST_BASE = "/comments/";
+
+
+// Mock Data for API
+import frontPageData from '../mockData/frontpage_response.json';
+import awwData from '../mockData/aww_response.json';
+import catsData from '../mockData/cats_response.json';
+import dogsData from '../mockData/dogs_response.json';
+import fishData from '../mockData/fish_response.json';
+import guineaPigsData from '../mockData/guinea_pigs_response.json';
 
 const RedditAPI = {
     // Format is as follows for posts <BASE_URL> <SUB_URL> <SUBREDDIT> <POST_BASE> <ID>
@@ -39,6 +47,75 @@ const RedditAPI = {
         const json = await res.json();
         const data = new Listing(JSON.stringify(json));
         return data;
+    },
+    fetchListing: (url, options) => {
+        const BASE_URL_REGEX = /^https:\/\/reddit\.com/g;
+        const SUB_URL_REGEX = /\/r\/\w*|\/(?!.)/g;
+        const DATA_FORMAT_REGEX = /(?=.+)(\.json)$/g;
+
+        return new Promise((resolve, reject) => {
+            const valid_base = BASE_URL_REGEX.test(url);
+            const valid_sub = SUB_URL_REGEX.test(url);
+            const valid_format = DATA_FORMAT_REGEX.test(url);
+
+            if (!valid_base | !valid_sub | !valid_format) {
+                reject(new Response(null, 
+                    {
+                        status: "400", 
+                        statusText: "Malformed URL", 
+                    }));
+            }
+
+            if (!(options["headers"] && options["headers"]["Content-Type"] === "application/json")) {
+                reject(new Response(null, {
+                    status: "406",
+                    statusText: "Not Acceptable Content Type"
+                }));
+            }
+
+            if (!(options["method"] && options["method"].toLowerCase() === "get")) {
+                reject(new Response(null, {
+                    status: "403",
+                    statusText: "Forbidden"
+                }));
+            }
+
+            const sub = url.match(SUB_URL_REGEX)[0].toLowerCase();
+            const successful_options = {
+                status: 200,
+                statusText: "OK",
+                headers: new Headers({
+                    "Content-Type": "application/json"
+                })
+            };
+
+            switch(sub) {
+                case "/": {
+                    resolve(new Response(JSON.stringify(frontPageData), successful_options));
+                    break;
+                }
+                case "/r/awww": {
+                    resolve(new Response(JSON.stringify(awwData), successful_options));
+                    break;
+                }
+                case "/r/cats": {
+                    resolve(new Response(JSON.stringify(catsData), successful_options));
+                    break;
+                }
+                case "/r/dogs": {
+                    resolve(new Response(JSON.stringify(dogsData), successful_options));
+                    break;
+                }
+                case "/r/fish": {
+                    resolve(new Response(JSON.stringify(fishData), successful_options));
+                    break;
+                }
+                case "/r/guineapigs": {
+                    resolve(new Response(JSON.stringify(guineaPigsData), successful_options));
+                    break;
+                }
+            }
+        });
     }
 };
 
