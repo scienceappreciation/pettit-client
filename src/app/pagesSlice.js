af4df9b1025ad50b4db0cce8bb651b74cb9a72b8
scienceappreciation <colehale059@gmail.com>
2023-10-04 19:16:57 -0500

    Cleanup Slice & Add to Store

--- src/app/pagesSlice.js
+++ src/app/pagesSlice.js
@@ -6,21 +6,6 @@ export const pagesSlice = createSlice({
         "/": [] // Front Page
     },
     reducers: {
-        increaseRequests: (state, action) => {
-            return {
-                ...state,
-                requests: state.requests + action.payload.increase
-            };
-        },
-        resetRequests: (state, action) => {
-            return {
-                ...state,
-                requests: 0
-            }
-        },
-        selectRequests: (state) => {
-            return state.requests
-        },
         cachePageOrder: (state, action) => {
             const { identifier, ids } = action.payload;
             return {
@@ -31,4 +16,4 @@ export const pagesSlice = createSlice({
     }
 });
 
-export const { increaseRequests, resetRequests, selectRequests, cachePageOrder } = pagesSlice.actions;
+export const { cachePageOrder } = pagesSlice.actions;
--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { postsSlice } from '../features/post/postsSlice';
+import { apiSlice } from '../features/api/apiSlice';
+import { pagesSlice } from './pagesSlice';
 
 export const store = configureStore({
   reducer: {
-    "posts": postsSlice.reducer
+    "posts": postsSlice.reducer,
+    "api": apiSlice.reducer,
+    "pages": pagesSlice.reducer
   }
 });
