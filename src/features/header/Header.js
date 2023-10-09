d9308ee5afc21b6afe43fb0b749286f4f59dbb14
scienceappreciation <colehale059@gmail.com>
2023-10-09 15:59:05 -0500

    Responsive Header

--- src/features/header/Header.js
+++ src/features/header/Header.js
@@ -5,7 +5,7 @@ function Header() {
     return (
         <header className='header'>
             <a class="aria-btn" href="#main-content" id="content-skip">Skip to Main Content</a>
-            <span className='demo-mark'>Static Demo</span>
+            <span className='demo-mark'>Demo</span>
             <h1 className='title'>Pettit</h1>
         </header>
     );
