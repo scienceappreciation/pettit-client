6a1f8c5b569b3a5609d46373b238a094f5ef711f
scienceappreciation <colehale059@gmail.com>
2023-10-09 14:19:41 -0500

    Added Static Demo Indicator

--- src/features/header/Header.js
+++ src/features/header/Header.js
@@ -4,6 +4,7 @@ import './Header.css';
 function Header() {
     return (
         <header className='header'>
+            <span className='demo-mark'>Static Demo</span>
             <span className='spacer'></span>
             <h1 className='title'>Pettit</h1>
             <span className='spacer'></span>
