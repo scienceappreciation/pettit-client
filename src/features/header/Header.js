fbf09d4211d512a16a29b1e2b3a7c7d9a509e28c
scienceappreciation <colehale059@gmail.com>
2023-10-09 15:37:16 -0500

    Header CSS Update

--- src/features/header/Header.js
+++ src/features/header/Header.js
@@ -4,11 +4,9 @@ import './Header.css';
 function Header() {
     return (
         <header className='header'>
-            <a class="aria-btn" href="#main-content">Skip to Main Content</a>
+            <a class="aria-btn" href="#main-content" id="content-skip">Skip to Main Content</a>
             <span className='demo-mark'>Static Demo</span>
-            <span className='spacer'></span>
             <h1 className='title'>Pettit</h1>
-            <span className='spacer'></span>
         </header>
     );
 }
