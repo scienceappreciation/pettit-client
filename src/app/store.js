3345435f488655d9c6fe9f82aeb125da34b9f7a1
scienceappreciation <colehale059@gmail.com>
2023-10-10 12:36:08 -0500

    Remove unused slice

--- src/app/store.js
+++ src/app/store.js
@@ -1,12 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { postsSlice } from '../features/post/postsSlice';
-import { apiSlice } from '../features/api/apiSlice';
 import { pagesSlice } from '../features/pages/pagesSlice';
 
 export const store = configureStore({
   reducer: {
     "posts": postsSlice.reducer,
-    "api": apiSlice.reducer,
     "pages": pagesSlice.reducer
   }
 });
